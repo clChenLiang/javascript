55e50b74b0b3c9e5ddccccbc12224d93dff9bf26
clchenliang <clchenliang@hotmail.com>
2018-02-16 00:07:48 +0800

    feat: 重构函数

--- typeScript_learn/index.js
+++ typeScript_learn/index.js
@@ -13,3 +13,14 @@ var p = {
     sex: 'false'
 };
 console.log('接口定义：', p);
+// function 
+function f1(param1, param2) {
+    return param2 === param1.toString();
+}
+console.log('function: ', f1(2, '3'));
+console.log('function -- 运行传异参', f1(3, f1(2, '3').toString()));
+function reverseFun(param1, param2) {
+    console.log('get the function params: ', typeof param1, typeof param2);
+}
+reverseFun(1, '3');
+reverseFun('1', 3);
--- typeScript_learn/index.ts
+++ typeScript_learn/index.ts
@@ -20,3 +20,20 @@ let p: person = {
     sex: 'false'
 }
 console.log('接口定义：',p);
+
+// function 
+function f1(param1:number, param2: string): boolean {
+    return param2 === param1.toString();
+}
+console.log('function: ', f1(2,'3'));
+console.log('function -- 运行传异参', f1(3, f1(2, '3').toString()));
+// 编译通过后，被引用的时候，传参并没有被限制
+
+// reverse function 重构函数
+function reverseFun(num: number, str: string): void;
+function reverseFun(str: string, num: number): void;
+function reverseFun(param1: string | number, param2: string | number) {
+    console.log('get the function params: ',typeof param1, typeof param2)
+}
+reverseFun(1, '3');
+reverseFun('1', 3);
